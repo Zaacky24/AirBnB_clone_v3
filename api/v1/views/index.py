dffa6f9f3acc9d8266ca80b1bef2a30bc401f073
DennisWanjeri <shikandadennis07@gmail.com>
2021-10-11 17:39:50 +0200

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,3 +15,23 @@ def status():
     if request.method == 'GET':
         resp = {"status": "OK"}
         return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    returns count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        classes_d = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in classes_d.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
