73f3d939e6be455ee561551a652c8cc047c2e9a6
DennisWanjeri <shikandadennis07@gmail.com>
2021-10-13 08:00:38 +0200

    cities and amenities

--- models/__init__.py
+++ models/__init__.py
@@ -11,7 +11,9 @@ storage_t = getenv("HBNB_TYPE_STORAGE")
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
+    CNC = DBStorage.CNC
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+    CNC = FileStorage.CNC
 storage.reload()
